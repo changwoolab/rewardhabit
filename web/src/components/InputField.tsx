999fc579cc8d0241686c1bc678c4c8ec348cb0f0
CWY <cwyoo01@naver.com>
2022-01-07 23:40:06 +0900

    길고 길었던 염원... InputField 난제가 풀렸다....

--- web/src/components/InputField.tsx
+++ web/src/components/InputField.tsx
@@ -1,39 +1,47 @@
-import { FormControl, FormErrorMessage, FormLabel, Input } from '@chakra-ui/react';
+import { Box, FormControl, FormLabel, Input, FormErrorMessage } from '@chakra-ui/react';
 import { Field, useField } from 'formik';
 import React, { InputHTMLAttributes } from 'react';
 
 function validateName(value) {
-    console.log(value);
     let error;
     if (!value) {
         error = 'Name is required'
-    } else if (value.toLowerCase() !== 'naruto') {
+    } else if (value.toString().toLowerCase() !== 'naruto') {
         error = "Jeez! You're not a fan 😱"
     }
     return error;
 }
 
-type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
-    label: string;
-    placeholder?: string;
+interface InputFieldtProps {
     name: string;
-    type?: string;
+    label: string;
+    type?: string
 }
 
-export const InputField: React.FC<InputFieldProps> = (props) => {
-    const [field, meta, helper] = useField(props);
+export const InputField: React.FC<InputFieldtProps> = (props) => {
     return (
-        <FormControl validate={validateName}>
-            {console.log(field, meta, helper)}
+        <Field name={props.name} validate={validateName}>
+            {() => <SubInputField {...props}/>}
+        </Field>
+    );
+}
+
+type SubInputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
+    label: string;
+    name: string;
+}
+
+export const SubInputField: React.FC<SubInputFieldProps> = ({label, size:_, ...props}) => {
+    const [field, {touched, error}] = useField(props);
+    props.placeholder = label;
+    return (
+        <Box mt={4}>
+        <FormControl isInvalid={touched && !!error}>
             <FormLabel htmlFor={field.name}>아이디</FormLabel>
-            <Input 
-                {...field}
-                id={field.name}
-                type={props.type}
-                label={props.label}
-                placeholder={props.placeholder} />
-            { meta.touched && meta.error ? <FormErrorMessage>{meta.error}</FormErrorMessage> : null }
+            <Input {...field} {...props} id={field.name} />
+            { touched && !!error ? <FormErrorMessage>{error}</FormErrorMessage> : null }
         </FormControl>
+        </Box>
     );
 }
 
