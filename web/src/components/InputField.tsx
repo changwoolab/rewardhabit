664d8ca16b1f72a190b69f99484ae36a1af001c2
Changwoo Yoo <cwyoo01@naver.com>
2022-01-14 23:17:16 +0900

    Select 추가

--- web/src/components/InputField.tsx
+++ web/src/components/InputField.tsx
@@ -1,4 +1,4 @@
-import { Box, FormControl, FormLabel, Input, FormErrorMessage, Textarea, ComponentWithAs, InputProps, TextareaProps } from '@chakra-ui/react';
+import { Box, FormControl, FormLabel, Input, FormErrorMessage, Textarea, Select } from '@chakra-ui/react';
 import { useField } from 'formik';
 import React, { InputHTMLAttributes } from 'react';
 
@@ -7,24 +7,38 @@ type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
     label: string;
     name: string;
     textarea?: boolean;
+    select?: boolean;
+    selectOptions?: {
+        value: string,
+        option: string,
+    }[]
 }
 
 
-export const InputField: React.FC<InputFieldProps> = ({label, size:_, textarea, ...props}) => {
+export const InputField: React.FC<InputFieldProps> = ({label, size:_, textarea, select, selectOptions, ...props}) => {
     const [field, {touched, error}] = useField(props);
     props.placeholder = label;
 
-    // input or Textarea를 결정하는 컴포넌트 형성
-    let InputOrTextarea: any = Input;
+    // input or Textarea or Select를 결정하는 컴포넌트 형성
+    let InputOrElse: any = Input;
+    let body: any = null;
     if (textarea) {
-        InputOrTextarea = Textarea;
+        InputOrElse = Textarea;
+    } else if (select) {
+        body = [];
+        InputOrElse = Select;
+        selectOptions?.map(values => {
+            body.push(<option key={values.value} value={values.value}>{values.option}</option>)
+        })
     }
 
     return (
         <Box mt={4}>
         <FormControl isInvalid={touched && !!error}>
             <FormLabel htmlFor={field.name}>{label}</FormLabel>
-            <InputOrTextarea {...field} {...props} id={field.name} />
+            <InputOrElse {...field} {...props} id={field.name} >
+                {body}
+            </InputOrElse>
             { touched && !!error ? <FormErrorMessage>{error}</FormErrorMessage> : null }
         </FormControl>
         </Box>
