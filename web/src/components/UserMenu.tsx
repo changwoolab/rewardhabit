7b6bf52e8b9b6d9aa3ca873e93acf1aaaee13465
Changwoo Yoo <cwyoo01@naver.com>
2022-01-17 16:20:50 +0900

    로그인 시 메뉴, 디자인 필요함

---
+++ web/src/components/UserMenu.tsx
@@ -0,0 +1,37 @@
+import { Text, Link, Menu, MenuButton, Button, MenuList, Center, MenuGroup, MenuItem, MenuDivider, useColorMode } from '@chakra-ui/react';
+import React from 'react';
+import { MeQuery, useLogoutMutation } from '../generated/graphql';
+
+interface UserMenuProps {
+    data: MeQuery;
+}
+
+export const UserMenu: React.FC<UserMenuProps> = ({ data }) => {
+    const [{fetching: logoutFetching}, logout] = useLogoutMutation();
+    
+    // 현재 다크 모드인지 확인
+    const { colorMode } = useColorMode()
+    const bgColor = colorMode === "dark" ? "white" : "black";
+
+    return (
+        <Menu>
+            <MenuButton as={Button} colorScheme='blue'>
+              {data.me?.partialUser?.userName}
+            </MenuButton>
+            <MenuList>
+            <Center>
+              <Text color={bgColor}>포인트</Text>
+            </Center>
+              <MenuGroup color={bgColor} title='프로필'>
+                <MenuItem color={bgColor}>내 프로필</MenuItem>
+                <MenuItem color={bgColor}>회원정보 수정</MenuItem>
+              </MenuGroup>
+              <MenuDivider />
+              <MenuGroup color={bgColor} title='독서록/일기'>
+                <MenuItem color={bgColor}>내가 쓴 글</MenuItem>
+              </MenuGroup>
+              <MenuItem><Link color={bgColor} onClick={() => logout()} isLoading={logoutFetching}>로그아웃</Link></MenuItem>
+            </MenuList>
+          </Menu>
+    );
+}
