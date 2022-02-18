8a4a8d1c3fb71a65b6673132eef5176e10657f41
Changwoo Yoo <cwyoo01@naver.com>
2022-02-18 15:21:12 +0900

    subscript

--- server/src/resolvers/subscript.ts
+++ server/src/resolvers/subscript.ts
@@ -1,9 +1,10 @@
 import { ReqResContext } from "../types/ReqResContext";
-import { Ctx, FieldResolver, Mutation, Resolver, Root, UseMiddleware } from "type-graphql";
+import { Arg, Ctx, FieldResolver, Mutation, Resolver, Root, UseMiddleware } from "type-graphql";
 import { Subscript } from "../entities/Subscript";
 import { getManager } from "typeorm";
 import { isAuth } from "../middleware/isAuth";
 import { InputType, Field } from "type-graphql";
+import { User } from "../entities/User";
 
 @InputType()
 export class DoSubscriptInput {
@@ -27,19 +28,46 @@ export class SubscriptResolver {
     @Root() subscript: Subscript,
     @Ctx() { req }: ReqResContext
   ) {
-    const res = await getManager().query(
-      `
-        select datediff(expireAt, startedAt) as dateDiff from subscript where userId = ?;
-        `,
-      [req.session.userId]
-    );
-    return res[0].dateDiff;
+    const today = new Date();
+    let diff = subscript.expireAt.getTime() - today.getTime();
+    diff = Math.ceil(diff / (1000 * 3600 * 24));
+    return diff <= 0 ? 0 : diff;
   }
 
   @Mutation(() => String)
   @UseMiddleware(isAuth)
-  async doSubscript() {
-    let date = new Date();
-    console.log(date);
+  async doSubscript(
+    @Arg("type", {nullable: true}) type: number | null,
+    @Arg("target") target: number,
+    @Arg("reward") reward: number,
+    @Arg("term") term: number,
+    @Ctx() { req }: ReqResContext
+  ) {
+    const { userId } = req.session;
+    const user = await User.findOne({id: userId})
+    // 1. 해당 유저에게 이미 구독이 있는지 확인
+    const existingSubscript = await Subscript.findOne({userId});
+    // 2. 구독한 적이 있다면
+    if (existingSubscript) {
+      // 2-1. 만료기간이 지난 경우 -> 기존구독 삭제 & 새 구독 삽입
+      const today = new Date();
+      let diff = existingSubscript.expireAt.getTime()-today.getTime();
+      diff = Math.ceil(diff / (1000 * 3600 * 24));
+      if (diff <= 0) {
+        
+      }
+      // 2-2. 만료기간이 지나지 않은 경우 -> throw Error
+
+      // 3. 구독중이지 않다면, 새로운 구독 삽입하기
+    } else {
+
+    }
+  }
+
+  /** 이미 구독중인지 확인해줌, 이미 구독중이면 결제 못하도록 함 */
+  @Mutation(() => String)
+  @UseMiddleware(isAuth)
+  async checkSubscript() {
+
   }
 }
--- web/src/pages/payment/success.tsx
+++ web/src/pages/payment/success.tsx
@@ -4,45 +4,65 @@ import React from 'react';
 import { Layout } from '../../components/Layout';
 import { createUrqlClient } from '../../utils/createUrqlClient';
 import http from "https"
+import { Button, Center } from '@chakra-ui/react';
 
 interface successProps {}
 
 const success: React.FC<successProps> = ({}) => {
     const router = useRouter();
     const {type, target, reward, term, paymentKey, orderId, amount} = router.query;
-    console.log(type, target, reward, term); console.log("paymentKey", paymentKey); console.log("orderId", orderId); console.log("amount", amount);
-    if (paymentKey && orderId && amount) {
-        const options = {
-            "method": "POST",
-            "hostname": "api.tosspayments.com",
-            "port": null,
-            "path": `/v1/payments/${paymentKey}`,
-            "headers": {
-            "Authorization": "Basic dGVzdF9za196WExrS0V5cE5BcldtbzUwblgzbG1lYXhZRzVSOg==",
-            "Content-Type": "application/json"
-            }
-        };
-        
-        const req = http.request(options, function (res) {
-            const chunks: any = [];
-            
-            res.on("data", function (chunk) {
-                chunks.push(chunk);
-            });
-            
-            res.on("end", function () {
-                const body = Buffer.concat(chunks);
-                console.log(body.toString());
-            });
-        });
-        
-        req.write(JSON.stringify({amount, orderId}));
-        req.end();
-    }
-    return (
-        <Layout variant="regular">
 
-        </Layout>
+    /** 보상습관 서버에 구독 정보 저장 */
+    const saveSubscriptToServer = async () => {
+        
+        return true;
+    }
+
+    /** Toss Payments로부터 결제승인 */
+    const getApproval = () => {
+      const options = {
+        method: "POST",
+        hostname: "api.tosspayments.com",
+        port: null,
+        path: `/v1/payments/${paymentKey}`,
+        headers: {
+          Authorization:
+            "Basic dGVzdF9za196WExrS0V5cE5BcldtbzUwblgzbG1lYXhZRzVSOg==",
+          "Content-Type": "application/json",
+        },
+      };
+
+      const req = http.request(options, function (res) {
+        const chunks: any = [];
+
+        res.on("data", function (chunk) {
+          chunks.push(chunk);
+        });
+
+        res.on("end", function () {
+          const body = Buffer.concat(chunks);
+          console.log(body.toString());
+        });
+      });
+
+      req.write(JSON.stringify({ amount, orderId }));
+      req.end();
+    };
+    return (
+      <Layout variant="regular">
+        <Center>
+          <Button
+            colorScheme={"teal"}
+            onClick={async () => {
+              const savedInServer = await saveSubscriptToServer();
+              if (savedInServer) getApproval();
+              else alert("오류가 발생했습니다");
+            }}
+          >
+            결제 완료하기
+          </Button>
+        </Center>
+      </Layout>
     );
 }
 
